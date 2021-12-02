73ae7ebced69a6e08db60341a7d83ca1cdc4b03b
Vishnu Sharma <vishnuds@umiacs.umd.edu>
2021-12-02 15:35:02 -0500

    Added prob conversion and scaling

--- dataloader/occmap_dataset.py
+++ dataloader/occmap_dataset.py
@@ -6,7 +6,7 @@ import torch.utils.data as data
 from torchvision import transforms
 
 class OccMapDataset(data.Dataset):
-    def __init__(self, filename='./description_ang0.csv', transform=None, input_dir='./inp_data/', target_dir='./gt_data/', mode='train'):
+    def __init__(self, filename='./description_ang0.csv', transform=None, input_dir='./inp_data/', target_dir='./gt_data/', mode='train', odds_to_prob=True, scale=10.):
         # Your code 
         df = pd.read_csv(filename)
         df = df[df['free_perc'] <= 80]
@@ -23,6 +23,10 @@ class OccMapDataset(data.Dataset):
         self.input_dir = input_dir
         self.target_dir = target_dir
 
+        self.odds_to_prob = odds_to_prob
+        self.scale = scale
+
+
     def __len__(self):
         # Your code 
         return len(self.filepaths)
@@ -34,9 +38,16 @@ class OccMapDataset(data.Dataset):
         tgt_img = np.load(f'{self.target_dir}/{self.filepaths[index]}.npy')
         
         data_dict = {'input image': inp_img[:, :], 'target image': tgt_img[ :, :]}
+        data_dict['input image'] *= self.scale
+        data_dict['target image'] *= self.scale
         
+        if self.odds_to_prob:
+            o2p_func = lambda x: np.exp(x)/(1. + np.exp(x))
+            data_dict['input image'] = o2p_func(data_dict['input image'])
+            data_dict['target image'] = o2p_func(data_dict['target image'])
+
         if self.transform is not None:
             data_dict['input image'] = self.transform(data_dict['input image'])
             data_dict['target image'] = self.transform(data_dict['target image'])
-        
+         
         return data_dict
